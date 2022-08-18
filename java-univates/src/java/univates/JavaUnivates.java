2495ae265b5f9c5ee9af414a31f81468e5d32017
FERNANDA OLIVEIRA HERNANDEZ <fernanda.hernandez@ACAD.UNIVATES>
2022-08-17 21:43:02 -0300

    Mensagem

---
+++ java-univates/src/java/univates/JavaUnivates.java
@@ -0,0 +1,24 @@
+
+package java.univates;
+
+public class JavaUnivates {
+
+   
+    public static void main(String[] args) {
+        
+        int idade = 26;
+        String nome = "Fernanda";
+        String cidade = "Lajeado";
+        
+        System.out.println("Nome:"+ nome +", Idade:" + idade +", Cidade:" + cidade);
+       
+       
+    }
+        
+}
+           
+        
+    
+    
+    
+
